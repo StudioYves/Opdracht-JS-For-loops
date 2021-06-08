a8a139d66d9b74b238561f64f495681d16ea5feb
Ivo Kersten <82444976+StudioYves@users.noreply.github.com>
2021-06-08 15:58:35 +0200

    Add files via upload

---
+++ even-odd-reporter.js
@@ -0,0 +1,8 @@
+for (number = 1; number <= 20 ; number++) {
+    if (number % 2 != 1) {
+        console.log(number, "is even")
+    }
+    else {
+        console.log(number, "is oneven")
+    }
+};
---
+++ multiplication-tables.js
@@ -0,0 +1,5 @@
+//MULTIPLICATION TABLES
+
+for (number = 1; number <= 10 ; number++) {
+    console.log(number, "x 9 =", number * 9);
+};
